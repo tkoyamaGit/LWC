8c8b7489127f82988dfdd3db4e72d15dbceae346
Takashi Koyama <takashi.koyama@biodesix.com>
2025-02-18 14:55:59 -0700

    Augmentor added

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,7 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Numerator extends LightningElement {
-    counter = 0;
+    @api counter = 0;
     handleIncrement() {
       this.counter++;
     }
@@ -15,5 +15,10 @@ export default class Numerator extends LightningElement {
     handleDivide(event) {
         const factor = event.detail;
         this.counter /= factor;
+    }
+
+    @api
+    maximizeCounter() {
+      this.counter += 1000000;
     }
 }
